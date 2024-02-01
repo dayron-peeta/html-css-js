a679197ab37b18097d12622c71b047d51427fd46
DPeeta <dayronalejandrocruzpita@gmail.com>
2024-02-01 16:12:52 -0500

    while-DoWhile

---
+++ JS/Introducción/cicloWhile.js
@@ -0,0 +1,14 @@
+//While similar al for pero con un solo condicional
+
+let contador = 0 
+while(contador <= 10){
+    console.log(contador)
+    contador++
+}
+
+//Do While se ejecuta el bloque de código al menos una vez
+let i = 100 
+do{
+    console.log(i)
+    i++
+} while(i <= 10 )
