43f2d408002700fcd06369bdacf39078d6205ccc
DPeeta <dayronalejandrocruzpita@gmail.com>
2024-02-02 11:29:26 -0500

    promesa7:29

---
+++ JS/Introducción/promesas2.js
@@ -0,0 +1,19 @@
+const fetchPost = () => {
+    return new  Promise((resolve, reject) => {
+        const posts = ['post1','post2']
+        const error = true;
+
+        if(error){ reject('fracasó')
+        } else { resolve({posts, mensaje:'éxito'})} //con varios valores, debes devolver un objeto o un arreglo en lugar de pasar múltiples argumentos a la función `resolve()`
+    })
+}
+
+fetchPost().then(({ posts, mensaje }) => {
+    console.log(mensaje)
+    console.log(posts)
+}).catch((error) => {  //se puede concatenar otro método
+    console.log(error)
+})
+
+// const miVariable = fetchPost()
+// console.log(miVariable) //si hacemos esto devuelve la promesa pero en estado pendiente
